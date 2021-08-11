dc870952bad4ea877c7d852db26a1405bb4e4802
Matías Valentín Muñoz Espinoza <writkas2@gmail.com>
2021-08-11 15:14:53 -0400

    Player changes (WIP)

--- Game/Actors/Player/Player.gd
+++ Game/Actors/Player/Player.gd
@@ -1,16 +1,32 @@
 extends KinematicBody2D
 
+class_name Player
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+var dir_y = 0
+var dir_rotation = 0
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	dir_rotation = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	dir_y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	
+	var x = 10 * sin(rotation)
+	var y = 10 * cos(rotation)
+
+	if dir_y == 1:
+#		move_and_slide(Vector2(0, dir_y * 100), position.rotated(rotation))
+		move_and_slide(Vector2(x, y))
+	elif dir_y == -1:
+#		move_and_slide(Vector2(0, dir_y * 100), position.rotated(rotation))
+		move_and_slide(Vector2(-x, -y))
+	
+	print_debug(rotation)
+	
+	if dir_rotation == 1:
+		rotation_degrees += dir_rotation * delta * 100
+	elif dir_rotation == -1:
+		rotation_degrees += -(dir_rotation * delta * 100)
+		
