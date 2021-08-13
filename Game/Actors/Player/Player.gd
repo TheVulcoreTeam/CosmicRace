9a3618139a542fd7388d9f1aa8b1e7b21f8361aa
Matías Valentín Muñoz Espinoza <writkas2@gmail.com>
2021-08-13 19:36:12 -0400

    remotesync

--- Game/Actors/Player/Player.gd
+++ Game/Actors/Player/Player.gd
@@ -4,17 +4,23 @@ class_name Player
 
 var dir_y = 0
 var dir_rotation = 0
+var speed = 100
+var speed_rotation = 100
 
 func _ready():
 	pass
 
 
 func _process(delta):
+	rpc_unreliable("move", delta)
+
+
+remotesync func move(delta):
 	dir_rotation = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
 	dir_y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
 	
-	var x = 10 * sin(rotation)
-	var y = 10 * cos(rotation)
+	var x = speed * sin(-rotation)
+	var y = speed * cos(-rotation)
 
 	if dir_y == 1:
 #		move_and_slide(Vector2(0, dir_y * 100), position.rotated(rotation))
@@ -26,7 +32,7 @@ func _process(delta):
 	print_debug(rotation)
 	
 	if dir_rotation == 1:
-		rotation_degrees += dir_rotation * delta * 100
+		rotation_degrees += dir_rotation * delta * speed_rotation
 	elif dir_rotation == -1:
-		rotation_degrees += -(dir_rotation * delta * 100)
+		rotation_degrees -= dir_rotation * delta * -speed_rotation
 		
